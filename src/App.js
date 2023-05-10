fd3b2cb966190422d69b6e5dc749f1d8a8a0cf37
Debanjan Malakar <92047662+Debanjan-Malakar@users.noreply.github.com>
2023-05-10 08:14:29 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ const Banner = styled.div`
   background: linear-gradient(159deg, rgb(45, 45, 58) 0%, rgb(43, 43, 53) 100%);
   height: 100vh;
   @media (max-width: 640px) {
-    height: 100%;
+    //height: 100%;
     padding-bottom: 2rem;
   }
 `;
